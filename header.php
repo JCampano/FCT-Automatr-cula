56895b68d1ee56c413bf0f9032ee64367ab84a96
DanielCazalla <34542666+DanielCazalla@users.noreply.github.com>
2018-04-19 15:41:54 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+      
+      <!-- CSS Personalizado -->
+       <link rel="stylesheet" href="css/style.css">
+
+    <title>Automatrícula</title>
+  </head>
+  <body>
+      <nav class="navbar navbar-light bg-light">
+          ¡Bienvenido!
+            <div class="text-right"><span class="navbar-text">
+        <a href="#">Registro</a> | <a href="#">Iniciar Sesión</a>
+    </span></div>
+          
+        </nav>
+      
+          
---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+    include "header.php";
+?>
+
+<div class="container-fluid">
+    
+    <div class="jumbotron margen-arriba">
+      <div class="container">
+        <h1 class="display-4">MatriculaT</h1>
+        <p class="lead">Bienvenido al gestor de matrículas del IES Hnos. Machado. Desde aquí, podrás gestionar las matrículas que posteriormente debes entregar en el centro.</p>
+      </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-4">
+            <a href="#">
+            <div class="card">
+                <img class="card-img-top" width="100" height="150" src="img/gris.png" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Registrar matrícula</h5>
+                  
+                  
+                </div>
+            </div>
+            </a>
+        </div>
+        <div class="col-sm-4">
+            <a href="#">
+            <div class="card">
+                <img class="card-img-top" width="100" height="150" src="img/gris.png" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Gestionar mis matrícula</h5>
+                  
+                  
+                </div>
+            </div>    
+            </a>
+        </div>
+        <div class="col-sm-4">
+            <a href="#">
+            <div class="card">
+                <img class="card-img-top" width="100" height="150" src="img/gris.png" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Ayuda y Documentación</h5>
+                  
+                  
+                </div>
+            </div>    
+            </a>
+        </div>
+    </div>
+</div>
+
+
+
+
+ <?php
+    include "footer.php";
+?>
