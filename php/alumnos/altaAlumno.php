80f089604f40cb59fde7c3553d958bd7ff84f686
JCampano <>
2018-04-22 20:40:03 +0200

    Actualización de la b.d y del backend de usuario

--- php/alumnos/altaAlumno.php
+++ php/alumnos/altaAlumno.php
@@ -1,5 +1,26 @@
 <?php
-include "../php/functions.php";
+include "../functions.php";
 extract($_POST);
-$consulta= "SELECT * FROM ALUMNOS WHERE DNI=".."";
+
+$consulta="SELECT * FROM ALUMNOS WHERE DNI='".$_POST['dni']."'";
+
+if(ejecutaConsulta2($consulta)!=0)
+{
+    echo ("Alumno ya registrado");
+}
+else
+{
+    $insert="INSERT INTO ALUMNOS (DNI, NOMBRE, APELLIDOS, CLAVE, ID_CENTRO, ID_MATRICULA) VALUES ('".$_POST['dni']."','".$_POST['nombre']."','".$_POST['apellidos']."','".$_POST['contrasena']."',0000,0000)";
+
+
+if(ejecutaConsultaAccion($insert)>0)
+{
+    echo ("Alta de alumno realizada");
+}
+else
+{
+    echo ("No se ha podido insertar");
+}
+}
+
 ?>
