ac66e6e2211d02117aa682f6e212e17e409dceaa
giampaslia <giampaslia1@gmail.com>
2018-04-24 20:01:30 +0200

    Add files via upload

--- php/form-login.php
+++ php/form-login.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Login</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-</head>
-
-<body>
     <div class="container-fluid">
     <div class = "row">
       <div class ="col-lg-12">
@@ -30,5 +22,3 @@
       </div>
     </div>
   </div>
-
-</html>
---
+++ php/form-registro.php
@@ -0,0 +1,36 @@
+  <div class="container-fluid">
+  <div class = "row">
+    <div class ="col-lg-12">
+      <form class="needs-validation" name="frmAltaAlumno" action="php/alumnos/altaAlumno.php" method="post" novalidate>
+          <div class="form-group">
+            <label for="dni" class="control-label">DNI</label>
+            <input type="text" class="form-control"  name="dni" placeholder="DNI" required>
+            <span class="invalid-feedback">Debe introducir el DNI</span>          
+        </div>
+
+          <div class="form-group">
+            <label for="nombre" class="control-label">Nombre</label>
+            <input type="text" class="form-control"  name="nombre" placeholder="Nombre" required>
+            <span class="invalid-feedback">Debe introducir un nombre</span>         
+        </div>  
+
+        <div class="form-group">
+            <label for="apellidos" class="control-label">Apellidos</label>
+            <input type="text" class="form-control"  name="apellidos" placeholder="Apellidos" required>
+            <span class="invalid-feedback">Debe introducir los Apellidos</span>         
+        </div>  
+
+          <div class="form-group">
+            <label for="contrasena" class="control-label">Contraseña</label>
+          <input type="password" class="form-control" id="contrasena" name="contrasena" placeholder="Contrase&ntilde;a" required>
+          <span class="invalid-feedback">Debe introducir una contrase&ntilde;a</span>
+          </div>
+
+          <div class="form-group">
+            <button type="submit" class="btn btn-primary ">Submit</button>
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>     
