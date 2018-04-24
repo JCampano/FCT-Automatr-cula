fb285fde70ca8ddb000773304934007d882e3266
JCampano <>
2018-04-24 13:22:58 +0200

    Backend de eliminar desde administrador estructurado.

--- php/alumnos/eliminarAlumnoAdministrador.php
+++ php/alumnos/eliminarAlumnoAdministrador.php
@@ -2,15 +2,15 @@
 include "../functions.php";
 extract($_POST);
 
-$consulta="SELECT * FROM ALUMNOS WHERE DNI='".$_SESSION['alumno']."'";
+$consulta="SELECT * FROM ALUMNOS WHERE DNI='".$_POST['dni']."'";
 
 if(ejecutaConsulta2($consulta)==0)
 {
-    echo ("El alumno no existe");
+    echo ("El alumno introducido no existe");
 }
 else
 {
-    $delete="DELETE FROM ALUMNOS WHERE DNI = '".$_SESSION['alumno']."'";
+    $delete="DELETE FROM ALUMNOS WHERE DNI = '".$_POST['dni']."'";
 
 
 if(ejecutaConsultaAccion($update)>0)
