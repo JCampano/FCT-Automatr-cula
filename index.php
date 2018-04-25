b19ddd75883f3cafba377eb4c7beb0e6f36e9e8b
giampaslia <giampaslia1@gmail.com>
2018-04-25 18:45:56 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-    include "header.php";
+session_start();//variable de sesion para controlar si hay error o no en el registro
+    include "header.php";       
+    if (isset($_SESSION['mensajeRegistro']) && isset($_SESSION['tipoMensaje']) ) {
+        echo '<div class="alert alert-'.$_SESSION['tipoMensaje'].' alert-dismissable">
+          <button type="button" class="close" data-dismiss="alert">&times;</button>'.$_SESSION["mensajeRegistro"].'</div>';
+        unset($_SESSION['mensajeRegistro']);
+        unset($_SESSION['tipoMensaje']);
+   }
 ?>
 
 <div class="container-fluid">
