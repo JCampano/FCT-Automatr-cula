b9534d7b1d565c662d9cd99701ad81201473e03f
giampaslia <giampaslia1@gmail.com>
2018-04-30 15:40:24 +0200

    Add files via upload

--- php/comprobar-login.php
+++ php/comprobar-login.php
@@ -1,13 +1,12 @@
 <?php
-
+session_start();
 include "functions.php";
 extract($_POST);
 
 $consulta="SELECT * FROM ALUMNOS WHERE DNI='".$_POST['dni']."' AND CLAVE='".$_POST['contrasena']."'";
 
 if(ejecutaConsulta2($consulta)!=0)
-{
-    session_start();
+{    
     $_SESSION["login"]=$_POST["dni"];
     $_SESSION["usuario"]="Consulta con el nombre pendiente";
     if(isset($_SESSION['ruta'])){
