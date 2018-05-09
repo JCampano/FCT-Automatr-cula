02f5f8cb53bd77e7794421f6b44b50c9112d172e
giampaslia <giampaslia1@gmail.com>
2018-05-09 17:57:12 +0200

    Add files via upload

--- php/form-registro.php
+++ php/form-registro.php
@@ -8,19 +8,19 @@ ob_start();
       <form class="needs-validation" name="frmAltaAlumno" action="php/alumnos/altaAlumno.php" method="post" novalidate>
           <div class="form-group">
             <label for="dni" class="control-label">DNI</label>
-            <input type="text" class="form-control"  name="dni" placeholder="DNI" required pattern="/^\d{8}\w$/">
+            <input type="text" class="form-control"  name="dni" placeholder="DNI" required pattern="\d{8}\w">
               <span class="invalid-feedback">El campo DNI debe constar de <strong>8 Números</strong> y <strong>1 Letra</strong></span>
           </div>
 
           <div class="form-group">
             <label for="nombre" class="control-label">Nombre</label>
-            <input type="text" class="form-control"  name="nombre" placeholder="Nombre" required pattern="/^[a-zA-Z\s]{3,40}$/">
+            <input type="text" class="form-control"  name="nombre" placeholder="Nombre" required pattern="[a-zA-Z\s]{3,40}">
             <span class="invalid-feedback">Introducir un nombre, <strong>Debe constar mínimo de 3 letras</strong></span>         
         </div>  
 
         <div class="form-group">
             <label for="apellidos" class="control-label">Apellidos</label>
-            <input type="text" class="form-control"  name="apellidos" placeholder="Apellidos" required pattern="/^[a-zA-Z\s]{3,40}$/">
+            <input type="text" class="form-control"  name="apellidos" placeholder="Apellidos" required pattern="[a-zA-Z\s]{3,40}">
             <span class="invalid-feedback">Introducir un Apellido, <strong>Debe constar mínimo de 3 letras</strong></span>         
         </div>  
 
