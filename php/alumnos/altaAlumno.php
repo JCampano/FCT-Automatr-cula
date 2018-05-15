bfa6eee2c62153d28617eb863e051cdf2dc0bd48
Usuario--GRH6 <grh6@ced.junta-andalucia.es>
2018-05-15 14:29:29 +0200

    En proceso.

--- php/alumnos/altaAlumno.php
+++ php/alumnos/altaAlumno.php
@@ -3,9 +3,35 @@ session_start();
 include "../functions.php";
 extract($_POST);
 
-$consulta="SELECT * FROM ALUMNOS WHERE DNI='".$_POST['dni']."'";
+$consulta="SELECT * FROM ALUMNOS WHERE DNI='".$dni."'";
+$dni=$_POST['dni'];
+$nombre=$_POST['nombre'];
+$apellidos=$_POST['apellidos'];
+$contrasena=$_POST['contrasena'];
+$valido=true;
 
+if(!preg_match("/[0-9]{7,8}[A-Z]/", $dni))
+{
+    $valido=false;
+}
 
+if(!preg_match("[a-zA-Z\s]{3,40}", $nombre))
+{
+    $valido=false;
+}
+/*
+if(!preg_match("[a-zA-Z\s]{3,40}", $apellidos))
+{
+    $valido=false;
+}
+
+if($contrasena=="")
+{
+    $valido=false;
+}
+*/
+if($valido==true)
+{
 if(ejecutaConsulta2($consulta)!=0)
 {
     $_SESSION['tipoMensaje']= "warning";
@@ -14,7 +40,7 @@ if(ejecutaConsulta2($consulta)!=0)
 }
 else
 {
-    $insert="INSERT INTO ALUMNOS (DNI, NOMBRE, APELLIDOS, CLAVE) VALUES ('".$_POST['dni']."','".$_POST['nombre']."','".$_POST['apellidos']."','".$_POST['contrasena']."')";
+    $insert="INSERT INTO ALUMNOS (DNI, NOMBRE, APELLIDOS, CLAVE) VALUES ('".$dni."','".$nombre."','".$apellidos."','".$contrasena."')";
 
 	if(ejecutaConsultaAccion($insert)>0)
 	{
@@ -29,5 +55,11 @@ else
 		header('Location: ../../index.php');	
 	}
 }
-
+}
+else
+{
+    $_SESSION['tipoMensaje']= "danger";
+    $_SESSION['mensajeRegistro'] = "<strong>Error</strong> al realizar la validación";
+    header('Location: ../../index.php');
+}
 ?>
--- php/comprobar-login.php
+++ php/comprobar-login.php
@@ -1,36 +1,10 @@
-<?php
+ <?php
 session_start();
 include "functions.php";
 extract($_POST);
 
-$valido=true;
-
 $dni=$_POST['dni'];
 $contrasena=$_POST['contrasena'];
-
-validar_dni($dni);
-validar_contrasena($contrasena);
-
-function validar_dni($dni)
-{
-	$letra = substr($dni, -1);
-	$numeros = substr($dni, 0, -1);
-	if (substr("TRWAGMYFPDXBNJZSQVHLCKE", $numeros%23, 1) != $letra && strlen($letra) != 1 && strlen ($numeros) != 8 ){
-		$valido=false;
-	}
-}
-
-function validar_contrasena($contrasena)
-{
-    if (trim($contrasena)=='') {
-
-        $valido=false;
-}
-}
-
-
-if($valido==true)
-{
 $consulta="SELECT * FROM ALUMNOS WHERE DNI='".$dni."' AND CLAVE='".$contrasena."'";
 $resulset=ejecutaConsulta($consulta);
 
@@ -51,12 +25,4 @@ $resulset=ejecutaConsulta($consulta);
         $_SESSION['sinLogin']="logueate";
         header('Location: ../index.php');
     }
-}
-else
-{
-    $_SESSION['tipoMensaje']= "danger";
-    $_SESSION['mensajeRegistro'] = "Validación incorrecta";
-    $_SESSION['sinLogin']="logueate";
-    header('Location: ../index.php');
-}
 ?>
