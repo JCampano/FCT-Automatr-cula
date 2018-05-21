826db6926be212c84b9701df2df5df7ba6c2a1ef
JCampano <>
2018-05-21 22:55:13 +0200

    Formulario de registro modificado.

--- php/form-registro.php
+++ php/form-registro.php
@@ -9,7 +9,7 @@ ob_start();
           <div class="form-group">
             <label for="dni" class="control-label">DNI</label>
             <input type="text" class="form-control"  name="dni" placeholder="DNI" required pattern="\d{8}\w">
-              <span class="invalid-feedback">El campo DNI debe constar de <strong>8 Números</strong> y <strong>1 Letra</strong></span>
+              <span class="invalid-feedback">El campo DNI debe constar de <strong>8 Números y 1 Letra</strong></span>
           </div>
 
           <div class="form-group">
@@ -21,93 +21,115 @@ ob_start();
           <div class="form-group">
             <label for="nombre" class="control-label">Nombre</label>
             <input type="text" class="form-control"  name="nombre" placeholder="Nombre" required pattern="[a-zA-Z\s]{3,40}">
-            <span class="invalid-feedback">Introducir un nombre, <strong>Debe constar mínimo de 3 letras</strong></span>         
+            <span class="invalid-feedback">Introducir un nombre <strong>debe constar mínimo de 3 letras</strong></span>
         </div>  
 
         <div class="form-group">
             <label for="apellido1" class="control-label">Primer apellido</label>
             <input type="text" class="form-control"  name="apellido1" placeholder="Primer apellido" required pattern="[a-zA-Z\s]{3,40}">
-            <span class="invalid-feedback">Introducir un Apellido, <strong>Debe constar mínimo de 3 letras</strong></span>         
+            <span class="invalid-feedback">Introducir un apellido <strong>debe constar mínimo de 3 letras</strong></span>
         </div>
 
         <div class="form-group">
             <label for="apellido2" class="control-label">Segundo Apellido</label>
             <input type="text" class="form-control"  name="apellido2" placeholder="Segundo Apellido" required pattern="[a-zA-Z\s]{3,40}">
-            <span class="invalid-feedback">Introducir un Apellido, <strong>Debe constar mínimo de 3 letras</strong></span>
+            <span class="invalid-feedback">Introducir un apellido <strong>debe constar mínimo de 3 letras</strong></span>
         </div>
 
         <div class="form-group">
             <label for="nie" class="control-label">NIE</label>
-            <input type="text" class="form-control"  name="nie" placeholder="NIE" required pattern="/^[XYZ][0-9]{7}[A-Z]$/i">
-              <span class="invalid-feedback">El campo NIE debe constar de <strong>7 números</strong> y <strong>dos letras</strong></span>
+            <input type="text" class="form-control"  name="nie" placeholder="NIE" required pattern="^[XYZ]\d{7,8}[A-Z]$">
+              <span class="invalid-feedback">El campo NIE debe constar de <strong>1 letra, 7 números y 1 letra</strong></span>
           </div>
 
           <div class="form-group">
               <label for="fecha_nac" class="control-label">Fecha Nacimiento</label>
-            <input type="text" class="form-control"  name="fecha_nac" placeholder="Fecha Nacimiento" required pattern="^\d{1,2}\/\d{1,2}\/\d{2,4}$">
-              <span class="invalid-feedback">El campo fecha debe tener el formato correcto. <strong>Ej: 3/09/1985</strong></span>
-          </div>
-          <!--
-          <div class="form-group">
-
+            <input type="date" class="form-control"  name="fecha_nac" placeholder="Fecha Nacimiento">
           </div>
 
           <div class="form-group">
-
+            <label for="direccion" class="control-label">Dirección</label>
+            <input type="text" class="form-control"  name="direccion" placeholder="Dirección" required pattern="[a-zA-Z\s]{3,40}">
+            <span class="invalid-feedback">Introducir una dirección <strong>debe constar mínimo de 3 letras</strong></span>
           </div>
 
           <div class="form-group">
-
+            <label for="poblacion" class="control-label">Población</label>
+            <input type="text" class="form-control"  name="poblacion" placeholder="Población" required pattern="[a-zA-Z\s]{3,40}">
+            <span class="invalid-feedback">Introducir una población <strong>debe constar mínimo de 3 letras</strong></span>
           </div>
 
           <div class="form-group">
-
+            <label for="cod_postal" class="control-label">Código postal</label>
+            <input type="text" class="form-control"  name="cod_postal" placeholder="Código postal" required pattern="^(?:0[1-9]\d{3}|[1-4]\d{4}|5[0-2]\d{3})$">
+            <span class="invalid-feedback">Introducir un código postal <strong>debe constar mínimo de 5 números comprendidos entre 01000 y 52999</strong></span>
           </div>
 
           <div class="form-group">
-
+            <label for="tel_fijo" class="control-label">Teléfono fijo</label>
+            <input type="text" class="form-control"  name="tel_fijo" placeholder="tel_fijo" required pattern="^\d{9}$">
+            <span class="invalid-feedback">Introducir un teléfono fijo <strong>debe constar de 9 dígitos</strong></span>
           </div>
 
           <div class="form-group">
-
+            <label for="tel_movil" class="control-label">Teléfono móvil</label>
+            <input type="text" class="form-control"  name="tel_movil" placeholder="tel_movil" required pattern="^\d{9}$">
+            <span class="invalid-feedback">Introducir un teléfono móvil <strong>debe constar de 9 dígitos</strong></span>
           </div>
 
           <div class="form-group">
-
+            <label for="e-mail" class="control-label">E-mail</label>
+            <input type="text" class="form-control"  name="e-mail" placeholder="e-mail" required pattern="^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$">
+            <span class="invalid-feedback">Debe introducir un e-mail válido</span>
           </div>
 
           <div class="form-group">
+            <label for="nombre_padre" class="control-label">Nombre del padre</label>
+            <input type="text" class="form-control"  name="nombre_padre" placeholder="Nombre del padre" required pattern="[a-zA-Z\s]{3,40}">
+            <span class="invalid-feedback">Introducir un nombre <strong>debe constar mínimo de 3 letras</strong></span>
+        </div>
 
+        <div class="form-group">
+            <label for="apellidos_padre" class="control-label">Apellidos del padre</label>
+            <input type="text" class="form-control"  name="apellidos_padre" placeholder="Apellidos del padre" required pattern="[a-zA-Z\s]{3,40}">
+            <span class="invalid-feedback">Introducir los apellidos <strong>debe constar mínimo de 3 letras</strong></span>
+        </div>
+
+        <div class="form-group">
+            <label for="tel_padre" class="control-label">Teléfono del padre</label>
+            <input type="text" class="form-control"  name="tel_padre" placeholder="Teléfono del padre" required pattern="^\d{9}$">
+            <span class="invalid-feedback">Introducir un teléfono <strong>debe constar de 9 dígitos</strong></span>
           </div>
 
           <div class="form-group">
+            <label for="e-mail_padre" class="control-label">E-mail del padre</label>
+            <input type="text" class="form-control"  name="e-mail_padre" placeholder="E-mail del padre" required pattern="^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$">
+            <span class="invalid-feedback">Debe introducir un e-mail válido</span>
+          </div>
 
+           <div class="form-group">
+            <label for="nombre_madre" class="control-label">Nombre de la madre</label>
+            <input type="text" class="form-control"  name="nombre_madre" placeholder="Nombre del madre" required pattern="[a-zA-Z\s]{3,40}">
+            <span class="invalid-feedback">Introducir un nombre <strong>debe constar mínimo de 3 letras</strong></span>
+        </div>
+
+        <div class="form-group">
+            <label for="apellidos_madre" class="control-label">Apellidos de la madre </label>
+            <input type="text" class="form-control"  name="apellidos_madre" placeholder="Apellidos de la madre" required pattern="[a-zA-Z\s]{3,40}">
+            <span class="invalid-feedback">Introducir los apellidos <strong>debe constar mínimo de 3 letras</strong></span>
+        </div>
+
+        <div class="form-group">
+            <label for="tel_madre" class="control-label">Teléfono de la madre</label>
+            <input type="text" class="form-control"  name="tel_padre" placeholder="Teléfono del madre" required pattern="^\d{9}$">
+            <span class="invalid-feedback">Introducir un teléfono <strong>debe constar de 9 dígitos</strong></span>
           </div>
 
           <div class="form-group">
-
+            <label for="e-mail_madre" class="control-label">E-mail de la madre</label>
+            <input type="text" class="form-control"  name="e-mail_madre" placeholder="E-mail del madre" required pattern="^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$">
+            <span class="invalid-feedback">Debe introducir un e-mail válido</span>
           </div>
-
-          <div class="form-group">
-
-          </div>
-
-          <div class="form-group">
-
-          </div>
-
-          <div class="form-group">
-
-          </div>
-
-          <div class="form-group">
-
-          </div>
-
-          <div class="form-group">
-
-          </div>
-          -->
 
           <div class="form-group">
             <button type="submit" class="btn btn-primary ">Enviar</button>
