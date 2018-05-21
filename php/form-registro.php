ec2efeed5a483e0508c713c94d362c6ad1e41f30
Usuario--GRH6 <grh6@ced.junta-andalucia.es>
2018-05-21 14:30:10 +0200

    Modificando formulario de registro.

--- php/form-registro.php
+++ php/form-registro.php
@@ -13,22 +13,102 @@ ob_start();
           </div>
 
           <div class="form-group">
+            <label for="contrasena" class="control-label">Contrase&ntilde;a</label>
+          <input type="password" class="form-control" name="contrasena" placeholder="Contrase&ntilde;a" required>
+          <span class="invalid-feedback">Debe introducir una contrase&ntilde;a</span>
+          </div>
+
+          <div class="form-group">
             <label for="nombre" class="control-label">Nombre</label>
             <input type="text" class="form-control"  name="nombre" placeholder="Nombre" required pattern="[a-zA-Z\s]{3,40}">
             <span class="invalid-feedback">Introducir un nombre, <strong>Debe constar mínimo de 3 letras</strong></span>         
         </div>  
 
         <div class="form-group">
-            <label for="apellidos" class="control-label">Apellidos</label>
-            <input type="text" class="form-control"  name="apellidos" placeholder="Apellidos" required pattern="[a-zA-Z\s]{3,40}">
+            <label for="apellido1" class="control-label">Primer apellido</label>
+            <input type="text" class="form-control"  name="apellido1" placeholder="Primer apellido" required pattern="[a-zA-Z\s]{3,40}">
             <span class="invalid-feedback">Introducir un Apellido, <strong>Debe constar mínimo de 3 letras</strong></span>         
-        </div>  
+        </div>
+
+        <div class="form-group">
+            <label for="apellido2" class="control-label">Segundo Apellido</label>
+            <input type="text" class="form-control"  name="apellido2" placeholder="Segundo Apellido" required pattern="[a-zA-Z\s]{3,40}">
+            <span class="invalid-feedback">Introducir un Apellido, <strong>Debe constar mínimo de 3 letras</strong></span>
+        </div>
+
+        <div class="form-group">
+            <label for="nie" class="control-label">NIE</label>
+            <input type="text" class="form-control"  name="nie" placeholder="NIE" required pattern="/^[XYZ][0-9]{7}[A-Z]$/i">
+              <span class="invalid-feedback">El campo NIE debe constar de <strong>7 números</strong> y <strong>dos letras</strong></span>
+          </div>
 
           <div class="form-group">
-            <label for="contrasena" class="control-label">Contrase&ntilde;a</label>
-          <input type="password" class="form-control" name="contrasena" placeholder="Contrase&ntilde;a" required>
-          <span class="invalid-feedback">Debe introducir una contrase&ntilde;a</span>
+              <label for="fecha_nac" class="control-label">Fecha Nacimiento</label>
+            <input type="text" class="form-control"  name="fecha_nac" placeholder="Fecha Nacimiento" required pattern="^\d{1,2}\/\d{1,2}\/\d{2,4}$">
+              <span class="invalid-feedback">El campo fecha debe tener el formato correcto. <strong>Ej: 3/09/1985</strong></span>
           </div>
+          <!--
+          <div class="form-group">
+
+          </div>
+
+          <div class="form-group">
+
+          </div>
+
+          <div class="form-group">
+
+          </div>
+
+          <div class="form-group">
+
+          </div>
+
+          <div class="form-group">
+
+          </div>
+
+          <div class="form-group">
+
+          </div>
+
+          <div class="form-group">
+
+          </div>
+
+          <div class="form-group">
+
+          </div>
+
+          <div class="form-group">
+
+          </div>
+
+          <div class="form-group">
+
+          </div>
+
+          <div class="form-group">
+
+          </div>
+
+          <div class="form-group">
+
+          </div>
+
+          <div class="form-group">
+
+          </div>
+
+          <div class="form-group">
+
+          </div>
+
+          <div class="form-group">
+
+          </div>
+          -->
+
           <div class="form-group">
             <button type="submit" class="btn btn-primary ">Enviar</button>
             <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
