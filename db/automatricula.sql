412e2dd74f22045eef2db399faa80b404cc15ac7
giampaslia <giampaslia1@gmail.com>
2018-06-08 20:36:33 +0200

    Update automatricula.sql

--- db/automatricula.sql
+++ db/automatricula.sql
@@ -290,8 +290,8 @@ ALTER TABLE `ensenanzas`
 -- Indices de la tabla `imagenes`
 --
 ALTER TABLE `imagenes`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_usuario` (`id_usuario`);
+  ADD PRIMARY KEY (`id`);
+  
 
 --
 -- Indices de la tabla `itinerarios`
