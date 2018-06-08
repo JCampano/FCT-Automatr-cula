c26f6da345d013b05c8bbe9a599ab7e50977ad10
giampaslia <giampaslia1@gmail.com>
2018-06-08 19:33:28 +0200

    Add files via upload

---
+++ php/matricula/editarMatricula.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+include "../functions.php";
+extract($_POST);
+
+$enseñanza=$_POST['selectEnseñanza'];
+$curso = $_POST['selectCurso'];
+$itinerario=$_POST['selectItinerario'];
+$optativa=$_POST['selectOptativas'];
+
+//obtenemos el id del alumno
+
+    $dni=$_SESSION['login'];    
+    $consulta="SELECT id FROM ALUMNOS WHERE DNI='".$dni."';";
+    $resulset=ejecutaConsulta($consulta);
+    $alumno=$resulset->fetch(PDO::FETCH_ASSOC); 
+
+$fecha1 = date("j-n-Y");
+$hora = date("H:i");   
+
+//echo $itinerario."-".$optativa;
+/*
+$consulta="SELECT * FROM matriculas WHERE id_alumno='".$alumno['id']."'";
+
+if(ejecutaConsulta2($consulta)==0)
+{
+    $_SESSION['tipoMensaje']= "warning";
+    $_SESSION['mensajeRegistro'] = "<strong>Error</strong> ,No tienes ninguna matricula dada de alta, vaya a generar matricula";
+    header('Location: ../../index.php');    
+}
+else{*/
+    $insert="UPDATE MATRICULAS SET NOMBRE = COD_MATRICULA, '$fecha', '$hora', '$alumno['id']', '$itinerario', '$optativa')";
+//}
+
+if(ejecutaConsultaAccion($insert)>0){
+    $_SESSION['tipoMensaje']= "warning";
+	$_SESSION['mensajeRegistro'] = "<strong>Matricula registrado con exito</strong>";
+	header('Location: ../../index.php');
+}
+else{	
+	$_SESSION['tipoMensaje']= "danger";
+	$_SESSION['mensajeRegistro'] = "<strong>Error</strong> al realizar el registro";
+	header('Location: ../../index.php');	
+}
+
+
+?>
