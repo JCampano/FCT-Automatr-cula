7c6d1695d94eaa0d1bcb5ab9026e7c173e01632a
giampaslia <giampaslia1@gmail.com>
2018-06-08 22:36:43 +0200

    Add files via upload

--- js/validacionFrm.js
+++ js/validacionFrm.js
@@ -6,14 +6,15 @@ $(function() {
     // Loop over them and prevent submission
     var validation = Array.prototype.filter.call(forms, function(form) {
       form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
+        if (form.checkValidity() === false || $("#contrasena").value != $("#contrasena2").value ) {
+        	alert("contraseñas distintas");
           event.preventDefault();
           event.stopPropagation();
-        }
+        }        
         form.classList.add('was-validated');
       }, false);
     });
-
+/*
     var pass1 = $("#contrasena").value;
 	var pass2 = $("#contrasena2").value;
     //alert(pass1);
@@ -24,31 +25,35 @@ $(function() {
 	//oculto por defecto el elemento span
 	var span = $('<span></span>').insertAfter(pass2);
 	span.hide();
+
+
 	//función que comprueba las dos contraseñas
 	function coincidePassword(){
-	var valor1 = pass1.val();
-	var valor2 = pass2.val();
-	//muestro el span
-	span.show().removeClass();
-	//condiciones dentro de la función
-	if(valor1 != valor2){
-	span.text(negacion).addClass('negacion');
-	}
-	if(valor1.length==0 || valor1==""){
-	span.text(vacio).addClass('negacion');
-	}
-	if(valor1.length<6 || valor1.length>10){
-	span.text(longitud).addClass('negacion');
-	}
-	if(valor1.length!=0 && valor1==valor2){
-	span.text(confirmacion).removeClass("negacion").addClass('confirmacion');
-	}
-	}
-	//ejecuto la función al soltar la tecla
-	pass2.keyup(function(){
-	coincidePassword();
-	});
-  }, false);
+		var pass1 = $("#contrasena").value;
+		var pass2 = $("#contrasena2").value;
+		var valor1 = pass1.val();
+		var valor2 = pass2.val();
+		//muestro el span
+		span.show().removeClass();
+		//condiciones dentro de la función
+		if(valor1 != valor2){
+		span.text(negacion).addClass('negacion');
+		}
+		if(valor1.length==0 || valor1==""){
+		span.text(vacio).addClass('negacion');
+		}
+		if(valor1.length<6 || valor1.length>10){
+		span.text(longitud).addClass('negacion');
+		}
+		if(valor1.length!=0 && valor1==valor2){
+		span.text(confirmacion).removeClass("negacion").addClass('confirmacion');
+		}
+		}
+		//ejecuto la función al soltar la tecla
+		pass2.keyup(function(){
+		coincidePassword();
+		});*/
+	  }, false);
 });
 
 
