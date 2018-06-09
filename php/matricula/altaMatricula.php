a9c735eaa8cf494b86fc1fc2b4f06cff5cb2fd19
giampaslia <giampaslia1@gmail.com>
2018-06-09 17:04:57 +0200

    Add files via upload

--- php/matricula/altaMatricula.php
+++ php/matricula/altaMatricula.php
@@ -50,15 +50,23 @@ $insert="INSERT INTO MATRICULAS (COD_MATRICULA,FECHA,HORA,ID_ALUMNO,ID_ITINERARI
 
 
 if(ejecutaConsultaAccion($insert)>0){
-	$insert="INSERT INTO OPTATIVAS_ELEGIDAS (COD_MATRICULA,ID_OPTATIVA,ID_OPTATIVA1,ID_OPTATIVA2,ID_OPTATIVA3) VALUES ('".$codigo."', '".$optativa."', '".$optativa2."', '".$optativa3."', '".$optativa4."')";
+	$insert="INSERT INTO OPTATIVAS_ELEGIDAS (COD_MATRICULA,ID_OPTATIVA1,ID_OPTATIVA2,ID_OPTATIVA3,ID_OPTATIVA4) VALUES ('".$codigo."', '".$optativa."', '".$optativa2."', '".$optativa3."', '".$optativa4."')";
 	if(ejecutaConsultaAccion($insert)>0){
-	    $_SESSION['tipoMensaje']= "success";
-		$_SESSION['mensajeRegistro'] = "<strong>Matricula registrado con exito</strong>";
-		header('Location: ../../index.php');
-	}else{
-		$_SESSION['tipoMensaje']= "danger";
-		$_SESSION['mensajeRegistro'] = "<strong>Error</strong> al guardar las optativas, por favor edite la matricula";
-		header('Location: ../../index.php');	
+		$consulta="SELECT * FROM MATRICULAS WHERE ID_ALUMNO='".$alumno['id']."';";
+	    $resulset=ejecutaConsulta($consulta);
+	    $matricula=$resulset->fetch(PDO::FETCH_ASSOC); 
+
+		$insert="INSERT INTO MATRICULAS_REGISTRADAS (ID_MATRICULA,ID_USUARIO,FECHA) VALUES ('".$matricula['id']."', '".$alumno['id']."', '".$fecha."')";
+		
+		if(ejecutaConsultaAccion($insert)>0){
+		    $_SESSION['tipoMensaje']= "success";
+			$_SESSION['mensajeRegistro'] = "<strong>Matricula registrado con exito</strong>";
+			header('Location: ../../index.php');
+		}else{
+			$_SESSION['tipoMensaje']= "danger";
+			$_SESSION['mensajeRegistro'] = "<strong>Error</strong> al guardar las optativas, por favor edite la matricula";
+			header('Location: ../../index.php');	
+		}
 	}
 }
 else{	
---
+++ php/matricula/eliminarMatricula.php
@@ -0,0 +1,33 @@
+<?php
+$id = $_GET['id'];
+$cod = $_GET['codMat'];
+
+session_start();
+
+include "../functions.php";
+
+//	ELIMINAMOS LAS OPTATIVAS
+$delete = "DELETE FROM OPTATIVAS_ELEGIDAS WHERE COD_MATRICULA= $cod";
+if(ejecutaConsultaAccion($delete)>0){
+
+	//ELIMIONAMOS LA MATRICULA REGISTRADA
+	$delete1 = "DELETE FROM MATRICULAS_REGISTRADAS WHERE ID_MATRICULA= $id";
+	if(ejecutaConsultaAccion($delete1)>0){
+
+		//ELIMIONAMOS LA MATRICULA 
+		$delete2 = "DELETE FROM MATRICULAS WHERE ID= $id";
+		if(ejecutaConsultaAccion($delete2)>0){
+			//MOSTRAMOS 
+		    $_SESSION['tipoMensaje']= "success";
+			$_SESSION['mensajeRegistro'] = "<strong>Matricula eliminada con exito</strong>";
+			header('Location: ../../index.php');
+		}
+	}
+}
+	else
+	{	
+		$_SESSION['tipoMensaje']= "danger";
+		$_SESSION['mensajeRegistro'] = "<strong>Error</strong> al Eliminar la Matricula";
+		header('Location: ../../index.php');	
+	}
+?>
