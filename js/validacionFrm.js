433b485f7a8acd9018fd9d2cf6cbfa52055c0382
JCampano <>
2018-06-09 20:39:35 +0200

    Error.

--- js/validacionFrm.js
+++ js/validacionFrm.js
@@ -1,59 +1,69 @@
-$(function() {
-  'use strict';
-  window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false || $("#contrasena").value != $("#contrasena2").value ) {
-        	alert("contraseñas distintas");
-          event.preventDefault();
-          event.stopPropagation();
-        }        
-        form.classList.add('was-validated');
-      }, false);
-    });
-/*
-    var pass1 = $("#contrasena").value;
-	var pass2 = $("#contrasena2").value;
-    //alert(pass1);
-	var confirmacion = "Las contraseñas si coinciden";
-	var longitud = "La contraseña debe estar formada entre 6-10 carácteres (ambos inclusive)";
-	var negacion = "No coinciden las contraseñas";
-	var vacio = "La contraseña no puede estar vacía";
-	//oculto por defecto el elemento span
-	var span = $('<span></span>').insertAfter(pass2);
-	span.hide();
+$(function () {
+    'use strict';
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+
+                if ($("#contrasena").value == $("#contrasena2").value) {
+                    $("#pass2").remove();
+                }
+
+                if (form.checkValidity() === false || $("#contrasena").value != $("#contrasena2").value) {
+
+                    if ($("#pass2").length == 0) {
+                        var invalid = "No coinciden las contraseñas";
+                        var pass2 = $("#contrasena2");
+                        var span = $('<span></span>').insertAfter(pass2);
+                        span.text(invalid).addClass('invalid-feedback');
+                        span.attr('id', 'pass2');
+                    }
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+        /*
+            var pass1 = $("#contrasena").value;
+        	var pass2 = $("#contrasena2").value;
+            //alert(pass1);
+        	var confirmacion = "Las contraseñas si coinciden";
+        	var longitud = "La contraseña debe estar formada entre 6-10 carácteres (ambos inclusive)";
+        	var negacion = "No coinciden las contraseñas";
+        	var vacio = "La contraseña no puede estar vacía";
+        	//oculto por defecto el elemento span
+        	var span = $('<span></span>').insertAfter(pass2);
+        	span.hide();
 
 
-	//función que comprueba las dos contraseñas
-	function coincidePassword(){
-		var pass1 = $("#contrasena").value;
-		var pass2 = $("#contrasena2").value;
-		var valor1 = pass1.val();
-		var valor2 = pass2.val();
-		//muestro el span
-		span.show().removeClass();
-		//condiciones dentro de la función
-		if(valor1 != valor2){
-		span.text(negacion).addClass('negacion');
-		}
-		if(valor1.length==0 || valor1==""){
-		span.text(vacio).addClass('negacion');
-		}
-		if(valor1.length<6 || valor1.length>10){
-		span.text(longitud).addClass('negacion');
-		}
-		if(valor1.length!=0 && valor1==valor2){
-		span.text(confirmacion).removeClass("negacion").addClass('confirmacion');
-		}
-		}
-		//ejecuto la función al soltar la tecla
-		pass2.keyup(function(){
-		coincidePassword();
-		});*/
-	  }, false);
+        	//función que comprueba las dos contraseñas
+        	function coincidePassword(){
+        		var pass1 = $("#contrasena").value;
+        		var pass2 = $("#contrasena2").value;
+        		var valor1 = pass1.val();
+        		var valor2 = pass2.val();
+        		//muestro el span
+        		span.show().removeClass();
+        		//condiciones dentro de la función
+        		if(valor1 != valor2){
+        		span.text(negacion).addClass('negacion');
+        		}
+        		if(valor1.length==0 || valor1==""){
+        		span.text(vacio).addClass('negacion');
+        		}
+        		if(valor1.length<6 || valor1.length>10){
+        		span.text(longitud).addClass('negacion');
+        		}
+        		if(valor1.length!=0 && valor1==valor2){
+        		span.text(confirmacion).removeClass("negacion").addClass('confirmacion');
+        		}
+        		}
+        		//ejecuto la función al soltar la tecla
+        		pass2.keyup(function(){
+        		coincidePassword();
+        		});*/
+    }, false);
 });
-
-
