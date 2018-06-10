1f3a1762875c3b1c46a243104ebc2c13a191f341
giampaslia <giampaslia1@gmail.com>
2018-06-10 17:48:40 +0200

    Add files via upload

--- php/matricula/eliminarMatricula.php
+++ php/matricula/eliminarMatricula.php
@@ -7,28 +7,40 @@ session_start();
 include "../functions.php";
 //echo $cod;
 
-//	ELIMINAMOS LAS OPTATIVAS
-$delete = "DELETE FROM OPTATIVAS_ELEGIDAS WHERE COD_MATRICULA='".$cod."'";
-if(ejecutaConsultaAccion($delete)>0){
+	$consulta="SELECT * FROM MATRICULAS_REGISTRADAS WHERE ID_USUARIO='".$id."';";  
 
-	//ELIMIONAMOS LA MATRICULA REGISTRADA
-	//$delete1 = "DELETE FROM MATRICULAS_REGISTRADAS WHERE ID_MATRICULA='". $id."'";
-	//if(ejecutaConsultaAccion($delete1)>0){
+   
+	//echo ejecutaConsultaAccion($consulta);
+    if(ejecutaConsultaAccion($consulta)>0)
+    {
+        $_SESSION['tipoMensaje']= "danger";
+        $_SESSION['mensajeRegistro'] = "<strong>Error</strong> No puedes Eliminar una matricula ya registrada";
+        header('Location: ../../index.php');    
+    }
+    else{
+		//	ELIMINAMOS LAS OPTATIVAS
+		$delete = "DELETE FROM OPTATIVAS_ELEGIDAS WHERE COD_MATRICULA='".$cod."'";
+		if(ejecutaConsultaAccion($delete)>0){
 
-	//ELIMIONAMOS LA MATRICULA 
-	$delete2 = "DELETE FROM MATRICULAS WHERE ID='". $id."'";
-	if(ejecutaConsultaAccion($delete2)>0){
-		//MOSTRAMOS 
-	    $_SESSION['tipoMensaje']= "success";
-		$_SESSION['mensajeRegistro'] = "<strong>Matricula eliminada con exito</strong>";
-		header('Location: ../../index.php');
-		
+			//ELIMIONAMOS LA MATRICULA REGISTRADA
+			//$delete1 = "DELETE FROM MATRICULAS_REGISTRADAS WHERE ID_MATRICULA='". $id."'";
+			//if(ejecutaConsultaAccion($delete1)>0){
+
+			//ELIMIONAMOS LA MATRICULA 
+			$delete2 = "DELETE FROM MATRICULAS WHERE ID='". $id."'";
+			if(ejecutaConsultaAccion($delete2)>0){
+				//MOSTRAMOS 
+			    $_SESSION['tipoMensaje']= "success";
+				$_SESSION['mensajeRegistro'] = "<strong>Matricula eliminada con exito</strong>";
+				header('Location: ../../index.php');
+				
+			}
+		}
+		else
+		{	
+			$_SESSION['tipoMensaje']= "danger";
+			$_SESSION['mensajeRegistro'] = "<strong>Error</strong> al Eliminar la Matricula";
+			header('Location: ../../index.php');	
+		}
 	}
-}
-else
-{	
-	$_SESSION['tipoMensaje']= "danger";
-	$_SESSION['mensajeRegistro'] = "<strong>Error</strong> al Eliminar la Matricula";
-	header('Location: ../../index.php');	
-}
 ?>
