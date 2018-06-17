dda03624dfe8761fac10bea9f75ee398956f7a9f
giampaslia <giampaslia1@gmail.com>
2018-06-17 13:55:58 +0200

    Add files via upload

--- php/matricula/getOptativa4.php
+++ php/matricula/getOptativa4.php
@@ -21,15 +21,15 @@ $conexion ->set_charset("utf8");//asi es el caracter utf8 si es msqli
 	$opt3 = $_GET['opt3'];
 
 	$sql="SELECT optativas.nombre,optativas.id FROM optativas,grupo_optativas WHERE grupo_optativas.id_curso='".$curso."' AND optativas.id_grupo_optativas=grupo_optativas.id";	
-	$sql.=" AND id.optativas !='".$opt1."'";
-	$sql.=" AND id.optativas !='".$opt3."'";
-	$sql.=" AND id.optativas !='".$opt2."';";
+	$sql.=" AND optativas.id !='".$opt1."'";
+	$sql.=" AND optativas.id !='".$opt3."'";
+	$sql.=" AND optativas.id !='".$opt2."';";
 	$res = $conexion->query($sql);
 	//montamos el codigo html del combo de cursos
 	//recorremos los cursos para crear el desplegable
 	
 	$respuesta='<label>Optativa 4</label><select class="custom-select" name="selectOptativas4" id="selectOptativas4"><option value="Seleccione">Seleccione</option>';
-	 
+	 //echo $sql;
 	    while($fila=mysqli_fetch_assoc($res)){
 	        $respuesta.='<option value="'.$fila['id'].'">';
 	        $respuesta.=$fila['nombre'];        
