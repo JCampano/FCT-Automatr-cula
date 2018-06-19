81e6c2942ad1bcb3cc3e31c636723ebb0f551331
JCampano <>
2018-06-19 23:24:02 +0200

    Cambios.

--- php/imprimirMatricula.php
+++ php/imprimirMatricula.php
@@ -7,7 +7,7 @@ ob_start();
 require_once 'matricula.php';
 $html = ob_get_clean();
 
-$nombreMatricula = "nombreParaLaMatricula.pdf";//es el nombre que se verá cuando el usuario le da a descargar documento
+$nombreMatricula = "matricula.pdf";//es el nombre que se verá cuando el usuario le da a descargar documento
 $pdf = new Html2Pdf('p','A4','es','true','UTF-8');
 $pdf->writeHTML($html);
 $pdf->output($nombreMatricula);
