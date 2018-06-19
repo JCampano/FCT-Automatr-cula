bbc7fd7138b56861b06b4b055382d6e80e60e902
DanielCazalla <danielcazalla3@gmail.com>
2018-06-19 23:23:03 +0200

    asdasd

--- panel/footer.php
+++ panel/footer.php
@@ -4,8 +4,7 @@
     <!-- Bootstrap Core JavaScript -->
     <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
 
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="vendor/metisMenu/metisMenu.min.js"></script>
+
 
 
 
